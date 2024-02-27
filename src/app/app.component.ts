a16a714ec193fabfb21c0d7ac6fe42f340c1a8a8
Tristan Andrei <tristanandrei.alipio@letran.edu.ph>
2024-02-27 12:10:26 +0800

    I love you 3000

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 })
 export class AppComponent {
-  title = 'Activity3';
+  title = "Databinding";
+  username = "";
+
+  GetData() {
+    return "Insert Username";
+  }
+
+  ChangeTitle(event) {
+    this.username = ""; // Reset username 
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {}
 }
